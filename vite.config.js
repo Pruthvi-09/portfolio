6710fed08a3cdae591516d3fb0c27eb9831c3290
Pruthviraj Sankpal <pruthvirajsankpal2003@gmail.com>
2026-03-22 15:01:41 +0530

    Fix vite manualChunks

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          'three': ['three'],
-          'react-three': ['@react-three/fiber', '@react-three/drei'],
-          'framer': ['framer-motion'],
+        manualChunks(id) {
+          if (id.includes('three')) {
+            return 'three'
+          }
+          if (id.includes('@react-three')) {
+            return 'react-three'
+          }
+          if (id.includes('framer-motion')) {
+            return 'framer'
+          }
         }
       }
     },
